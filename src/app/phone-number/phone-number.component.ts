c41983e73c4ed4b9682825c2327d53c972813195
dshorter <codesurfer@gmail.com>
2020-03-20 13:26:48 -0400

    1 file modified

--- src/app/phone-number/phone-number.component.ts
+++ src/app/phone-number/phone-number.component.ts
@@ -16,14 +16,10 @@ export class PhoneNumberComponent implements OnInit {
   }
 
   OnKeyUp(event: any  ) {
-    //  alert(event.target.value)  ;
-
+    
     this.phone = event.target.value;
-    let regStr: any  = "^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$" ;
-    let regexpNumber: RegExp =  regStr ;
- alert(regexpNumber )
+    let regexpNumber: RegExp =  new RegExp("^(\\+\\d{1,2}\\s)?\\(?\\d{3}\\)?[\\s.-]\\d{3}[\\s.-]\\d{4}$" )    ;
     this.result = regexpNumber.test(this.phone) ;
-    alert(regexpNumber.test(this.phone) )
     if (regexpNumber.test(this.phone) == true  ){
          this.result =  'Pass!'
     } else {
