c6a4b46858e2419104acd1ba743b36ce6cdc1a32
dshorter <codesurfer@gmail.com>
2020-04-01 14:16:58 -0400

    1 file modified

--- src/app/validators/phone-nanp.directive.ts
+++ src/app/validators/phone-nanp.directive.ts
@@ -1,29 +1,24 @@
-
-import { Validator, NG_VALIDATORS, FormControl } from '@angular/forms'
-import { Directive, OnInit, forwardRef } from '@angular/core';
-
+import { Validator, NG_VALIDATORS, FormControl } from "@angular/forms";
+import { Directive, OnInit, forwardRef } from "@angular/core";
 
 @Directive({
-  selector: '[gteValidator]',
+  selector: "[phoneNanp]",
   providers: [
-    { provide: NG_VALIDATORS, useExisting: gteValidatorDirective, multi: true }
+    { provide: NG_VALIDATORS, useExisting: phoneNanpDirective, multi: true }
   ]
 })
-export class gteValidatorDirective implements Validator, OnInit {
-
-  ngOnInit() {
-  }
+export class phoneNanpDirective implements Validator, OnInit {
+  ngOnInit() {}
 
   validate(c: FormControl) {
-
     let v: number = +c.value;
 
     if (isNaN(v)) {
-      return { 'gte': true, 'requiredValue': 10 }
+      return { gte: true, requiredValue: 10 };
     }
 
     if (v <= +10) {
-      return { 'gte': true, 'requiredValue': 10 }
+      return { gte: true, requiredValue: 10 };
     }
 
     return null;
